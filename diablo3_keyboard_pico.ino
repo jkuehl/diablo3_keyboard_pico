ddc3afe518a549cf3ab55747c12fe2f880e92098
John Kuehl <rayben@gmail.com>
2024-12-07 08:51:46 +0100

    Text update.

--- diablo3_keyboard_pico.ino
+++ diablo3_keyboard_pico.ino
@@ -1,4 +1,4 @@
-// The old version of the Arduino IDE (at the moment 1.8.19) is required, the 2.x version does not upload to my Picos correctly.
+// The old version of the Arduino IDE (at the moment 1.8.19) is required, the 2.x version does not upload to my Picos correctly (for me!).
 
 // Basic idea was taken from the Arduino KeyboardAndMouseControl sketch, thanks for the very good example <3. Add this "additional Boards Manager
 // URL": https://github.com/earlephilhower/arduino-pico/releases/download/global/package_rp2040_index.json and switch to Pi Pico and Compile. See
@@ -6,12 +6,11 @@
 
 #include <Keyboard.h>
 #include <Mouse.h>
-// These are included the board libs.
+// The board libs include these includes.
+
 
 void setup() {
-  // make pin 2 an input and turn on the
-  // pullup resistor so it goes LOW unless
-  // connected to ground:
+  // Make the pin an input and turn on the pullup resistor, so it goes LOW unless connected to ground.
   pinMode(0, INPUT_PULLUP); // mouse left
   pinMode(1, INPUT_PULLUP); // mouse right
   pinMode(2, INPUT_PULLUP); // button 1
@@ -25,14 +24,13 @@ void setup() {
 }
 
 void loop() {
-  // Keep in mind the pull-up means the pushbutton's logic is inverted. It goes HIGH when it's open, and LOW when it's pressed. Turn on pin 13 when the button's pressed, and off when it's not:
+  // Sorting is upside down to have all other keys interrupt the mainly used (almost permanently pressed) left mouse-click.
   if (digitalRead(7) == LOW) { Keyboard.print("z");      delay(77); }
   if (digitalRead(6) == LOW) { Keyboard.print("q");      delay(77); }
   if (digitalRead(5) == LOW) { Keyboard.print("4");      delay(77); }
   if (digitalRead(4) == LOW) { Keyboard.print("3");      delay(77); }
   if (digitalRead(3) == LOW) { Keyboard.print("2");      delay(77); }
   if (digitalRead(2) == LOW) { Keyboard.print("1");      delay(77); }
-  // Sorting is upside down to have other keys interrupt left and right mouse-click via the keyboard.
   if (digitalRead(1) == LOW) { Keyboard.print("q");      delay(77); }
   // if (digitalRead(1) == LOW) { Mouse.click(MOUSE_RIGHT); delay(77); }
   if (digitalRead(0) == LOW) { Mouse.click(MOUSE_LEFT);  delay(77); }
